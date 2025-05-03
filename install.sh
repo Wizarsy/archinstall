767e93c03ac6a6f5ceb14f40267ae5cdf47c0542
Wizarsy <filipemadruga@outlook.com>
2025-05-03 18:03:47 -0300

    change: update var names

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-TIMEZONE=America/Sao_Paulo
-USERNAME=wizarsy
-LOCALE=en_US
-HOSTNAME=VM
-DEVICE=/dev/sda
-LANG=en_US.UTF-8
-KEYMAP=us
+export __TIMEZONE=America/Sao_Paulo \
+  __USERNAME=wizarsy \
+  __LOCALE=en_US \
+  __HOSTNAME=VM \
+  __DEVICE=/dev/sda \
+  __LANG=en_US.UTF-8 \
+  __KEYMAP=us
 
-parted "$DEVICE" --script mklabel gpt
-parted "$DEVICE" --script mkpart "boot" fat32 1MiB 1025MiB 
-parted "$DEVICE" --script mkpart "swap" linux-swap 1025MiB 5121MiB 
-parted "$DEVICE" --script mkpart "root" ext4 5121MiB 100%
-parted "$DEVICE" --script set 1 boot on
+parted "$__DEVICE" --script mklabel gpt
+parted "$__DEVICE" --script mkpart "boot" fat32 1MiB 1025MiB 
+parted "$__DEVICE" --script mkpart "swap" linux-swap 1025MiB 5121MiB 
+parted "$__DEVICE" --script mkpart "root" ext4 5121MiB 100%
+parted "$__DEVICE" --script set 1 boot on
 
-mkfs.fat -F 32 "${DEVICE}1"
-mkswap "${DEVICE}2"
-mkfs.ext4 "${DEVICE}3"
+mkfs.fat -F 32 "${__DEVICE}1"
+mkswap "${__DEVICE}2"
+mkfs.ext4 "${__DEVICE}3"
 
-mount "${DEVICE}3" /mnt
-mount --mkdir "${DEVICE}1" /mnt/boot
-swapon "${DEVICE}2"
+mount "${__DEVICE}3" /mnt
+mount --mkdir "${__DEVICE}1" /mnt/boot
+swapon "${__DEVICE}2"
 
 pacstrap -K /mnt base linux linux-firmware sudo
 
@@ -29,32 +29,32 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
 
-ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+ln -sf /usr/share/zoneinfo/"$__TIMEZONE" /etc/localtime
 hwclock --systohc
 
-sed -i "/${LOCALE}/s/^#//" /etc/locale.gen
-locale-gen
+sed -i "/${__LOCALE}/s/^#//" /etc/__LOCALE.gen
+__LOCALE-gen
 
-echo "LANG=${LANG}" > /etc/locale.conf
-echo "KEYMAP=${KEYMAP}" > /etc/vconsole.conf
+echo "__LANG=${__LANG}" > /etc/__LOCALE.conf
+echo "__KEYMAP=${__KEYMAP}" > /etc/vconsole.conf
 
-echo "$HOSTNAME" > /etc/hostname
+echo "$__HOSTNAME" > /etc/__HOSTNAME
 cat << EOF > /etc/hosts
 127.0.0.1  localhost
 ::1        localhost
-127.0.1.1  $HOSTNAME
+127.0.1.1  $__HOSTNAME
 EOF
 
-useradd -m -G wheel -s /bin/bash $USERNAME
+useradd -m -G wheel -s /bin/bash $__USERNAME
 passwd root
-passwd $USERNAME
-echo "$USERNAME ALL=(ALL) ALL" > /etc/sudoers.d/00_$USERNAME
+passwd $__USERNAME
+echo "$__USERNAME ALL=(ALL) ALL" > /etc/sudoers.d/00_$__USERNAME
 
-pacman -S --no-confirm grub efibootmgr
-grub-install "${DEVICE}1"
+pacman -S --noconfirm grub efibootmgr
+grub-install "${__DEVICE}1"
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S --no-confirm dhcpcd networkmanager resolvconf
+pacman -S --noconfirm dhcpcd networkmanager resolvconf
 systemctl enable dhcpcd
 systemctl enable NetworkManager
 systemctl enable systemd-resolved
